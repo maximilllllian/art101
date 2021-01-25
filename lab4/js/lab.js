997e63c91aebc91594d4bad94a4951abae7cb41a
maximilllllian <77122082+maximilllllian@users.noreply.github.com>
2021-01-25 10:13:45 -0800

    Update lab.js

---
+++ lab4/js/lab.js
@@ -0,0 +1,27 @@
+//Task 1: How to Make Kimchi
+  //Gather ingredients and supplies
+  //Pour salt and leave for 30 mins/ 1 hour
+  //Pour bowl into a strainer and drain out water
+  //Return cabbage to bowl
+  //Combine garlic, ginger, shallot, and Korean chili flakes in a blender
+  //Run blender
+  //Optional:
+    //Cut carrot, radish, and scallion into small pieces
+  //Combine cabbage, paste, and optional vegetables in bowl
+  //Put on gloves
+  //Mix ingredients by hand
+  //Pack the contents of bowl into large jar
+  //Leave 1-2 inches of room at the top of jar
+  //Cover with a lid but do not screw it on tight
+  //Set in a cool and dark place for 3-5 days
+  //Enjoy!
+
+//Task 2: Google Chrone T-Rex Game
+  //Check user’s internet connection
+  //Load start screen if the user has no internet connection
+  //Start game if user presses space
+  //Jump if player presses space
+  //Duck if player presses down arrow
+  //Increase score
+  //Increase speed every time user’s score reaches an interval of 100
+  //Stop score and display “Game Over” text if user hits an obstacle
