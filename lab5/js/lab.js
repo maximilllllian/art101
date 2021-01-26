c5dfa289a2fbceda316115b4ac9971a02381aeaa
maximilllllian <77122082+maximilllllian@users.noreply.github.com>
2021-01-25 23:22:17 -0800

    Update lab.js

---
+++ lab5/js/lab.js
@@ -0,0 +1,15 @@
+
+//define skateboard variables
+const make = "skateboard";
+const model = "creature";
+const color = "blue and orange";
+const year = 2020;
+const ownIt = true;
+const age = 2021 - year;
+
+console.log(make);
+console.log(model);
+console.log(color);
+console.log(year);
+console.log(ownIt);
+console.log(age);
