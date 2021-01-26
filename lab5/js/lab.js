241f883e3bd86787bed269066d4d9d63fea7ee2c
maximilllllian <77122082+maximilllllian@users.noreply.github.com>
2021-01-25 23:33:46 -0800

    Update lab.js

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,19 +1,22 @@
 //define skateboard variables
 const make = "skateboard";
 const model = "creature";
-const color = "blue and orange";
+const color = "green and blue";
 const year = 2020;
 const ownIt = true;
 const age = 2021 - year;
 
+//writes in document
 document.writeIn("Make: " + make);
 document.writeIn("Model: " + model);
 document.writeIn("Color: " + color);
 document.writeIn("Year: " + year);
+//if-else statement :)
 if (ownIt) {
   document.writeIn("Owns it?: yes");
 }
 else {
   document.writeIn("Owns it?: no");
 }
+
 document.writeIn("Age: " + age);
