823e819e0aa35755ee9d7f73be37f0d86e839f35
maximilllllian <77122082+maximilllllian@users.noreply.github.com>
2021-02-07 18:03:57 -0800

    Create anagram.js

---
+++ lab7/js/anagram.js
@@ -0,0 +1,16 @@
+// we want to create a string with a random shuffle without reusing letters
+//create a function to shuffle the users name to create an anagram
+//
+//turn string into an old array
+
+//start with a new blank array
+
+//until the old array is empty,
+
+//  we draw a random element from old array
+
+//  add it to new array
+
+//turn new array into string
+
+//trim blank off beginning or end
