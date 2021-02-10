269bfdeec315eb987f62920e1644be60725e6592
maximilllllian <77122082+maximilllllian@users.noreply.github.com>
2021-02-09 20:06:18 -0800

    Create lab.js

---
+++ lab9/js/lab.js
@@ -0,0 +1,14 @@
+//find output and assign to variable outputEl
+//create new element and assign to variable new1El
+//create new element and assign to variable new2El
+var outputEl = document.getElementById("output");
+var new1El = document.createElement('div');
+var new2El = document.createElement('div');
+//change HTML attribute to say 'this is a new element'
+//change HTML attribute to say 'this is a new new element'
+new1El.innerHTML = 'this is a new element';
+new2El.innerHTML = 'this is a new new element';
+
+//document.getElementById();
+document.body.appendChild(new1El);
+document.body.appendChild(new2El);
