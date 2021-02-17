770c4c23e97554b1895b9c1e8576844849eab3f1
maximilllllian <77122082+maximilllllian@users.noreply.github.com>
2021-02-17 10:28:06 -0800

    Update lab.js

--- lab11/js/lab.js
+++ lab11/js/lab.js
@@ -2,22 +2,21 @@
 // Author: Maximilian Breall
 
 //create first button
-var buttonEl = $("challenge-button");
-$("challenge-button").click(function() {
-
-
-  });
+var challengeDiv = $('#challenge')
+challengeDiv.append('<button id = challenge-button>Click me!</button>');
 //create second button
-var buttonEl = $("problems-button");
-$("problems-button").click(function() {
-
-  });
+var problemsDiv = $('#problems')
+problemsDiv.append('<button id = problems-button>Click me!</button>');
 //create third button
-var buttonEl = $("results-button");
-$("results-button").click(function() {
+var resultsDiv = $('#results')
+resultsDiv.append('<button id = results-button>Click me!</button>');
 
+//Event listeners
+$('#challenge-button').click(function(){
+  $('#challenge').toggleClass('special');
+});
 
-
+/*
     var inputEl = document.getElementById('title-text');
     var changeTitle = inputEl.value;
     var newTitle = document.getElementById('title');
@@ -25,7 +24,7 @@ $("results-button").click(function() {
     //document.body.appendChild(titleEl);
 
 });
-
+*/
 //   var nameEl = document.getElementById('user-name');
 //   var userName = nameEl.value;
 //   var sortedUserName = userName.split('').sort().join('');
