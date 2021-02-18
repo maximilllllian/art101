9e6c2f6cc76cd7c1aeb7144bcf380c9fc5d306ae
maximilllllian <77122082+maximilllllian@users.noreply.github.com>
2021-02-17 18:22:22 -0800

    Update lab.js

--- lab11/js/lab.js
+++ lab11/js/lab.js
@@ -14,21 +14,15 @@ resultsDiv.append('<button id = results-button>Click me!</button>');
 //Event listeners
 $('#challenge-button').click(function(){
   $('#challenge').toggleClass('special');
+  $('#problems-button').toggleClass('challenge-button');
 });
 
-/*
-    var inputEl = document.getElementById('title-text');
-    var changeTitle = inputEl.value;
-    var newTitle = document.getElementById('title');
-    newTitle.innerText = "Lab 10: " + changeTitle;
-    //document.body.appendChild(titleEl);
-
+$('#problems-button').click(function(){
+  $('#problems').toggleClass('special');
+  $('#results-button').toggleClass('challenge-button');
 });
-*/
-//   var nameEl = document.getElementById('user-name');
-//   var userName = nameEl.value;
-//   var sortedUserName = userName.split('').sort().join('');
-//   console.log(sortedUserName);
-//   var outputEl = document.getElementById('output');
-//   outputEl.innerText = "Your user name: " + sortedUserName;
-// });
+
+$('#results-button').click(function(){
+  $('#results').toggleClass('special');
+  $('#challenge-button').toggleClass('challenge-button');
+});
