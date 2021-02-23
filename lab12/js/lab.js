855b9daa2b9fc934c10697fa8782761c4bd1775b
maximilllllian <77122082+maximilllllian@users.noreply.github.com>
2021-02-22 17:27:21 -0800

    Update lab.js

--- lab12/js/lab.js
+++ lab12/js/lab.js
@@ -9,31 +9,22 @@ function sortingHat (str) {
   var mod = len % 4;
   //create a conditional to show house depending on mod
   if (mod == 0) {
-    return "Gryffindor";
+    return "Gryffindor. Gryffindor values bravery, daring, nerve, and chivalry. Its emblematic animal is the lion, and its colors are scarlet and gold.";
     } else if (mod == 1){
-    return "Ravenclaw";
+    return "Ravenclaw. ";
     } else if (mod == 2){
     return "Slytherin";
     } else if (mod == 3){
-    return "Hufflepuff";
+    return "Hufflepuff. Hufflepuff values hard work, dedication, patience, loyalty, and fair play. Its emblematic animal is the badger.";
     };
   };
 //create an event listener attached to #button
 $("#button").click(function(){
 //get the value in the #input text box, assign to variable name
-  var name = $("#input");
+  var name = $("#input").val();
         //   //get the length of the name
                 // var nameLength = name.length;
 // run sortingHat(name) and store the result in a variable house
   var house = sortingHat(name);
-  if (house == "Gryffindor") {
-    $("#output").append("<p>The Sorting Hat has sorted you into Gryffindor. Gryffindor values bravery, daring, nerve, and chivalry." +
-    "Its emblematic animal is the lion, and its colors are scarlet and gold.</p>");
-  } else if (house == "Ravenclaw"){
-    $("#output").append("<p> The Sorting Hat has sorted you into Ravenclaw. Ravenclaw ")
-  } else if (house == "Slytherin"){
-    $("#output").append()
-  } else if (house == "Hufflepuff"){
-    $("#output").append()
-  };
+  $("#output").append('<p>The Sorting Hat has sorted you into ' + house);
 });
