2b66d84d18dd09a9ad056f5f7ccc7c49995ed0ac
maximilllllian <77122082+maximilllllian@users.noreply.github.com>
2021-03-08 19:53:58 -0800

    asd d

--- lab15/js/lab.js
+++ lab15/js/lab.js
@@ -35,3 +35,38 @@ $('#submit').click(function(){
 
 
 });
+
+$('#NASA').click(function(){
+  var apiURL = "https://apod.nasa.gov/apod/image/2103/VolcanoStars_Vella_1080.jpg"
+  console.log(apiURL);
+
+  $.ajax({
+    // The URL for the request (from the api docs)
+    url: apiURL,
+    // The data to send (will be converted to a query string)
+    data: {
+            // here is where any data required by the api
+            //   goes (check the api docs)
+            //id: 123,
+            //api_key: "blahblahblah",
+          },
+    // Whether this is a POST or GET request
+    type: "GET",
+    // The type of data we expect back
+    dataType : "json",
+    // What do we do when the api call is successful
+    //   all the action goes in here
+    success: function(data) {
+        // do stuff
+        $('#photo').html(data.extract_html);
+        console.log(data);
+      },
+    // What we do if the api call fails
+    error: function (jqXHR, textStatus, errorThrown) {
+        // do stuff
+        console.log("Error:", textStatus, errorThrown);
+    }
+  })
+
+
+});
---
+++ lab16/js/lab.js
@@ -0,0 +1,42 @@
+//Maximilian Breall
+//March 8, 2021
+
+// Using the core $.ajax() method
+var comicObj = {};
+
+function getAndPutData(apiURL){$.ajax({
+    // The URL for the request (from the api docs)
+    url: apiURL,
+    // The data to send (will be converted to a query string)
+    data: { },
+    // Whether this is a POST or GET request
+    type: "GET",
+    // The type of data we expect back
+    dataType : "json",
+    // What do we do when the api call is successful
+    //   all the action goes in here
+    success: function(data) {
+        // do stuff
+        comicObj = data;
+        $('#output-image').html("<h2>" + data.title + "</h2>" +"<img src = '" + data.img + "' title= '" + data.alt + "'>");
+    },
+    // What we do if the api call fails
+    error: function (jqXHR, textStatus, errorThrown) {
+        // do stuff
+        console.log("Error:", textStatus, errorThrown);
+    }
+});
+}
+getAndPutData("https://xkcd.com/614/info.0.json");
+
+$('#backward').click(function(){
+  var comicNum = comicObj.num - 1;
+  var newURL = "https://xkcd.com/" + comicNum + "/info.0.json";
+  getAndPutData(newURL);
+});
+
+$('#forward').click(function(){
+  var comicNum = comicObj.num + 1;
+  var newURL = "https://xkcd.com/" + comicNum + "/info.0.json";
+  getAndPutData(newURL);
+});
